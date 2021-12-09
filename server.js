fb945129afddd4acf074c07bf7ceab1a003cbb9c
adhipratama <yogaadhi@icloud.com>
2021-12-09 22:07:06 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express();
+const bcrypt = require('bcrypt')
+
+const users = [];
+
+app.set('view-engine', 'ejs')
+app.unsubscribe(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {name: 'yoga'})
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', (req, res) => {
+    
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', async (req, res)=>{
+   try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+   }catch{
+
+   }
+   
+    req.body.email
+})
+
+app.listen(5000)
