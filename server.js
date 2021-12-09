bdc10b00097110c1acda995182ed415107b24e90
adhipratama <yogaadhi@icloud.com>
2021-12-09 22:46:07 +0700

    still doesnt work

--- server.js
+++ server.js
@@ -1,36 +1,41 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 
 const users = [];
 
-app.set('view-engine', 'ejs')
-app.unsubscribe(express.urlencoded({extended: false}))
+app.set("view-engine", "ejs");
+app.unsubscribe(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.render('index.ejs', {name: 'yoga'})
-})
+app.get("/", (req, res) => {
+  res.render("index.ejs", { name: "yoga" });
+});
 
-app.get('/login', (req, res) => {
-    res.render('login.ejs')
-})
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
 
-app.post('/login', (req, res) => {
-    
-})
+app.post("/login", (req, res) => {});
 
-app.get('/register', (req, res) => {
-    res.render('register.ejs')
-})
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
 
-app.post('/register', async (req, res)=>{
-   try{
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-   }catch{
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect("/login");
+  } catch(err) {
+    res.send(err);
+    // res.redirect("/register.ejs");
+  }
+  console.log(users);
+});
 
-   }
-   
-    req.body.email
-})
-
-app.listen(5000)
+app.listen(5000);
